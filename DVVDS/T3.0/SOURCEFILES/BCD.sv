da3c17c3ae7643f9634e6c6dbff0a1497e581d06
SebassOR <Sebass_mx@outlook.com>
2026-08-31 18:20:39 -0600

    word con imagenes de prueba

--- DVVDS/T3.0/SOURCEFILES/BCD.sv
+++ DVVDS/T3.0/SOURCEFILES/BCD.sv
@@ -1,8 +1,62 @@
-module BCD (
-	input logic [7:0] binary,
+module BCD_Converter #(
+	parameter [7:0] bin = 8'd0
+)(
 	
-	output logic [3:0] BCD0,
-	output logic [3:0] BCD1,
-	output logic [3:0] BCD2
+	output logic [3:0] units,
+	output logic [3:0] tens,
+	output logic [3:0] hundreds
 );
 
+	logic [19:0] stage [0:8];
+
+	assign stage[0] = {12'b0, bin};
+
+	stp_BCD step0 
+		(.INs(stage[0]), 
+		.OUTs(stage[1]));
+	stp_BCD step1 
+		(.INs(stage[1]), 
+		.OUTs(stage[2]));
+	stp_BCD step2 
+		(.INs(stage[2]), 
+		.OUTs(stage[3]));
+	stp_BCD step3 
+		(.INs(stage[3]), 
+		.OUTs(stage[4]));
+	stp_BCD step4 
+		(.INs(stage[4]), 
+		.OUTs(stage[5]));
+	stp_BCD step5 
+		(.INs(stage[5]), 
+		.OUTs(stage[6]));
+	stp_BCD step6 
+		(.INs(stage[6]), 
+		.OUTs(stage[7]));
+	stp_BCD step7 
+		(.INs(stage[7]), 
+		.OUTs(stage[8]));
+
+			assign units    = stage[8][11:8];
+			assign tens     = stage[8][15:12];
+			assign hundreds = stage[8][19:16];
+
+endmodule
+
+module stp_BCD (
+	input  logic [19:0] INs,
+	output logic [19:0] OUTs
+);
+
+	logic [3:0] units_adj, tens_adj, hundreds_adj;
+
+	always_comb begin
+		// add-3 si el dígito actual es >= 5
+		units_adj    = (INs[11:8]  >= 5) ? (INs[11:8]  + 4'd3) : INs[11:8];
+		tens_adj     = (INs[15:12] >= 5) ? (INs[15:12] + 4'd3) : INs[15:12];
+		hundreds_adj = (INs[19:16] >= 5) ? (INs[19:16] + 4'd3) : INs[19:16];
+
+		// shift left de todo el registro (digitos ajustados + bits de binario)
+		OUTs = {hundreds_adj, tens_adj, units_adj, INs[7:0]} << 1;
+	end
+
+endmodule
--- DVVDS/T3.0/SOURCEFILES/top.sv
+++ DVVDS/T3.0/SOURCEFILES/top.sv
@@ -1,14 +1,11 @@
 module top #(
-	//parameter [7:0] bin = 8'd000
+	parameter [7:0] bin = 8'd000
 	//parameter [7:0] bin = 8'd042
 	//parameter [7:0] bin = 8'd064
 	//parameter [7:0] bin = 8'd144
 	//parameter [7:0] bin = 8'd164
 	//parameter [7:0] bin = 8'd255
-
 )(
-
-	input  logic [7:0] binary,
 	output logic [6:0] segments_hundreds,
 	output logic [6:0] segments_tens,
 	output logic [6:0] segments_units
@@ -17,24 +14,22 @@ module top #(
 	logic [3:0] bcd_units, bcd_tens, bcd_hundreds;
 
 	
-	BCD_Converter u_bcd (
-		.binary  (binary),
+	BCD_Converter #(.bin(bin)) u_bcd (
 		.units   (bcd_units),
 		.tens    (bcd_tens),
 		.hundreds(bcd_hundreds)
 	);
 
-	
-	SevenSegDecoder u_seg_units    
-	(.num(bcd_units),   
-	.seg(segments_units));
-	
-	SevenSegDecoder u_seg_tens    
-	(.num(bcd_tens),     
-	.seg(segments_tens));
-	
-	SevenSegDecoder u_seg_hundreds 
-	(.num(bcd_hundreds), 
-	.seg(segments_hundreds));
+	Segments u_seg_units
+		(.num(bcd_units),
+		.seg(segments_units));
+
+	Segments u_seg_tens
+		(.num(bcd_tens),
+		.seg(segments_tens));
+
+	Segments u_seg_hundreds
+		(.num(bcd_hundreds),
+		.seg(segments_hundreds));
 
 endmodule

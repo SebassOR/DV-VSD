fbcda0e3c51aa0e3c04e45d6743b586814319044
SebassOR <Sebass_mx@outlook.com>
2026-09-21 00:16:00 -0600

    Instancia de modulo de uart rx en el tb

--- DVVDS/T5.0/SOURCEFILES/UART_RX_TB.sv
+++ DVVDS/T5.0/SOURCEFILES/UART_RX_TB.sv
@@ -1,7 +1,27 @@
 `timescale 1ns/1ps
 
 module uart_rx_tb;
+    localparam int    TB_CLKS_PER_BIT = 5;
+    localparam string TB_PARITY_MODE  = "EVEN"; 
+    logic       clk = 0;
+    logic       rst;
+    logic       rx = 1'b1; 
+    logic [7:0] rx_data;
+    logic       received;
+    logic       parity_error;
 
+
+     uart_rx #(
+        .CLKS_PER_BIT(TB_CLKS_PER_BIT),
+        .PARITY_MODE("EVEN")
+    ) dut (
+        .clk          (clk),
+        .rst          (rst),
+        .rx           (rx),
+        .rx_data      (rx_data),
+        .received     (received),
+        .parity_error (parity_error)
+    );
 
 
 endmodule
